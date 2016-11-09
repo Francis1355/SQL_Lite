288f37951ddfd0399fbf14b7bdeef9bbd9e962c8
Francis1355 <franciscastillo.1355@gmail.com>
2016-11-08 21:34:05 -0700

    Initial commit

---
+++ app/src/main/java/com/example/francisco/sql_lite/AdminSQLiteOpenHelper.java
@@ -0,0 +1,31 @@
+package com.example.francisco.sql_lite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+
+/**
+ * Created by Francisco on 01/11/2016.
+ */
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                                 int version) {
+        super(context, name, factory, version);
+
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table articulos(codigo int primary key, descripcion text, precio real)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
+
+
